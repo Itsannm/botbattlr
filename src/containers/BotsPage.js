f9cf0a929f149471bae3cfded197261e9d14f611
Itsannm <ann.mwanzia@student.moringaschool.com>
2023-08-02 15:52:36 +0300

    deployment

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -7,126 +7,118 @@ import BotSearch from '../components/BotSearch'
 import Filter from '../components/Filter'
 
 class BotsPage extends React.Component {
-  constructor(){
-    super()
+  constructor() {
+    super();
     this.state = {
       allBots: [],
       selectBot: undefined,
       query: '',
       filter: 'All'
-    }
+    };
   }
 
-  componentDidMount(){
+  componentDidMount() {
     fetch('https://dbjson-bot-com.onrender.com/bots')
-    .then(res => res.json())
-    .then(bots => this.setBots(bots))
+      .then(res => res.json())
+      .then(bots => this.setBots(bots))
       .then(bots => this.setState({
         allBots: bots
-      }))
+      }));
   }
 
   setBots = (bots) => {
     return bots.map(bot => {
-      bot.owned = false
-      return bot
-    })
+      bot.owned = false;
+      return bot;
+    });
   }
 
   clickBot = (bot) => {
     this.setState({
       selectBot: bot
-    })
+    });
   }
 
   addBot = (selectBot) => {
     let x = this.state.allBots.map(bot => {
-      if(bot.id === selectBot.id){
-        bot.owned = !bot.owned
-        return bot
-      }else {
-        return bot
+      if (bot.id === selectBot.id) {
+        bot.owned = !bot.owned;
       }
-    })
+      return bot;
+    });
     this.setState({
       allBots: x
-    })
+    });
+  }
+
+  removeBotFromArmy = (bot) => {
+    let x = this.state.allBots.map(b => {
+      if (b.id === bot.id) {
+        b.owned = false;
+      }
+      return b;
+    });
+    this.setState({
+      allBots: x
+    });
   }
 
   filterFreeBots = () => {
-    let freeBots = []
-    this.state.allBots.map(bot => {
-      if(bot.owned === false){
-        freeBots.push(bot)
-      }
-    })
-    if(this.state.filter !== 'All'){
-      freeBots = freeBots.filter(bot => 
-        bot.bot_class == this.state.filter
-      )
+    let freeBots = this.state.allBots.filter(bot => bot.owned === false);
+    if (this.state.filter !== 'All') {
+      freeBots = freeBots.filter(bot => bot.bot_class === this.state.filter);
     }
-    if(this.state.query){
-      freeBots = freeBots.filter(bot=> 
-        bot.name.toLowerCase().includes(this.state.query.toLowerCase())
-      )
+    if (this.state.query) {
+      freeBots = freeBots.filter(bot => bot.name.toLowerCase().includes(this.state.query.toLowerCase()));
     }
-    return freeBots
+    return freeBots;
   }
 
   filterOwnedBots = () => {
-    let ownedBots = []
-    this.state.allBots.map(bot => {
-      if(bot.owned === true){
-        ownedBots.push(bot)
-      }
-    })
-    let filtered = ownedBots.filter(bot=> {
-      return bot.name.toLowerCase().includes(this.state.query.toLowerCase())
-    })
-    return filtered
+    let ownedBots = this.state.allBots.filter(bot => bot.owned === true);
+    let filtered = ownedBots.filter(bot => bot.name.toLowerCase().includes(this.state.query.toLowerCase()));
+    return filtered;
   }
 
   handleClear = () => {
     this.setState({
       query: ''
-    })
+    });
   }
 
   handleChange = (query) => {
     this.setState({
       query: query
-    })
+    });
   }
 
   clearSpec = () => {
     this.setState({
       selectBot: undefined
-    })
+    });
   }
 
   filterChange = (value) => {
     this.setState({
       filter: value
-    })
+    });
   }
 
-  
   render() {
-    console.log(this.state)
+    console.log(this.state);
     return (
       <div className="ui segment inverted olive bot-army">
-        <Header/>
-        <BotSearch handleClear={this.handleClear} handleChange={this.handleChange}/>
+        <Header />
+        <BotSearch handleClear={this.handleClear} handleChange={this.handleChange} />
         <br></br>
-        <Filter filterChange={this.filterChange}/>
-        <YourBotArmy bots={this.filterOwnedBots()} addBot={this.clickBot}/>
+        <Filter filterChange={this.filterChange} />
+        <YourBotArmy bots={this.filterOwnedBots()} addBot={this.clickBot} removeBot={this.removeBotFromArmy} />
         <br></br>
-        {this.state.selectBot ? <BotSpecs bot={this.state.selectBot} clearSpec={this.clearSpec} addBot={this.addBot} />: 
-          <BotsCollection bots={this.filterFreeBots()} addBot={this.clickBot}/>}
+        {this.state.selectBot ? <BotSpecs bot={this.state.selectBot} clearSpec={this.clearSpec} addBot={this.addBot} /> :
+          <BotsCollection bots={this.filterFreeBots()} addBot={this.clickBot} />}
       </div>
     );
   }
-
 }
 
 export default BotsPage;
